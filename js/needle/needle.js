323e8b7b6b287eff88765c4210e5e9cb7b3b6333
Ben Belchak <ben@needle.com>
2010-05-13 12:33:37 -0600

    Fixed up the javascript a little bit.

--- js/needle/needle.js
+++ js/needle/needle.js
@@ -1,10 +1,11 @@
-Validation.add('validate-rand_len', 'String length must be between 5 and 10.', {
-       min : 5,
-       max : 10,
-       include : ['validate-digits']
-    });
-
-Validation.add('validate-quantity', 'Cannot be more than 400.', {
-       max : 400,
-       include : ['validate-digits']
-    });
+Validation.addAllThese([
+	['validate-rand_len', 'String length must be between 5 and 10.', {
+		min : 5,
+		max : 10,
+		include : ['validate-digits']
+	}],
+	['validate-quantity', 'Cannot be more than 400.', {
+		max : 400,
+		include : ['validate-digits']
+	}]
+]);
