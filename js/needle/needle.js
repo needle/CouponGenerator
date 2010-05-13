7283645eb8d8162a5abf9899ad3c8434b1da0ca6
Ben Belchak <ben@needle.com>
2010-05-13 12:20:17 -0600

    PHP seems to time out after about 430 coupons generated.  The error is a blank page or sometimes a maximum time exceeded error. To stop this from happening temporarily, I've limited the user to only generating 400 coupons at once.

--- js/needle/needle.js
+++ js/needle/needle.js
@@ -3,3 +3,8 @@ Validation.add('validate-rand_len', 'String length must be between 3 and 10.', {
        max : 10,
        include : ['validate-digits']
     });
+
+Validation.add('validate-quantity', 'Cannot be more than 400.', {
+       max : 400,
+       include : ['validate-digits']
+    });
